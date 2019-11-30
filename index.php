cbbd431df34938c4f70a6f37112473722b08cc0a
bagasamri <bagas.amri7@gmail.com>
2019-11-30 16:27:14 +0000

    ini adalah hasil karya bagasamri gooing goo

---
+++ index.php
@@ -0,0 +1,167 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!--My fonts _-->
+    <link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+    <!-- My css-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+    <title>Going</title>
+  </head>
+  <body>
+   <!-- Navbar-->
+
+   <nav class="navbar navbar-expand-lg navbar-light">
+   	<div class="container">
+		  <a class="navbar-brand" href="#">Going Goo</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    <div class="navbar-nav ml-auto">
+		      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+		      <a class="nav-item nav-link" href="#">Pricing</a>
+		      <a class="nav-item nav-link" href="#">Features</a>
+		      <a class="nav-item nav-link" href="#"  >About</a>
+		      <a class="nav-item btn btn-primary tombol" href="#" >Join Us</a>
+		    </div>
+		  </div>
+    </div>
+</nav>
+
+   <!-- akhir navbar-->
+
+   <!-- JUMBOTRON -->
+			<div class="jumbotron jumbotron-fluid">
+			  <div class="container">
+			    <h1 class="display-4">Join Us whith a <span>Comfortable</span><br>and <span>Enjoyable</span> trip with us</h1>
+			    <a href="" class="btn btn-primary tombol"> Out Traveling </a>
+			  </div>
+			</div>
+
+   <!--AKHIR JUMBO TRON-->
+
+<div class="container">
+	
+	<!--info panel -->
+<div class="row justify-content-center">
+	<div class="col-10 info-panel">
+		<div class="row">
+			<div class="col-lg">
+				<img src="img/pesona.jpg" alt="Employee" class="float-left">
+				<h4>Pantai</h4>
+				<p>lorem ipsum dolor site amet.</p>
+			</div>
+			<div class="col-lg">
+				<img src="img/gunung.jpg" alt="Employee" class="float-left">
+				<h4>Gunung Rinjani</h4>
+				<p>lorem ipsum dolor site amet.</p>
+			</div>
+			<div class="col-lg">
+				<img src="img/kampung.jpg" alt="Employee" class="float-left">
+				<h4>Perkampungan Lombok</h4>
+				<p>lorem ipsum dolor site amet.</p>
+			</div>
+			
+		</div>
+		
+	</div>
+	
+</div>
+
+	<!--akir info pannel-->
+
+
+<!--workingspace-->
+<div  class="row workingspace">
+	<div class="col-lg-6">
+		<img src="img/pantailombok.jpg" alt="Workingspace" class="img-fluid">
+	</div>
+	<div class="col-lg-5">
+			<h3> You <span>work</span> like at <span> Traveling</span></h3>
+
+			<p> Pantai ini terkenal di lombok tepatnya dibagian utara lombok. </p>
+
+		 <a href="" class="btn btn-primary tombol">Galery</a>
+	</div>
+</div>
+
+
+<!--akhir workingspace-->
+
+ <!--Testimonial-->
+ <section class="testimonial">
+ 	<div class="row justify-content-center">
+ 		<div class="col-lg-8">
+ 			<h5>"" Traveling dengan suasana hati yang kebih asik dan mempelajarin hal baru setiap Perjalananya.""</h5>
+ 		</div>
+ 		
+ 	</div>
+
+
+
+ 	<div class="row justify-content-center">
+ 		<div class="col-lg-6 justify-content-center d-flex">
+ 			<figure class="figure">
+			  <img src="img/gety.jpg" class="figure-img img-fluid rounded-circle" alt="Testi 1">
+			</figure>
+ 			<figure class="figure">
+			  <img src="img/cici.jpg" class="figure-img img-fluid rounded-circle utama" alt="Testi 2">
+			  <figcaption class="figure-caption">
+			  	<h5>Cece oyy</h5>
+			  	<p>Penyanyi</p>
+			  </figcaption>
+			</figure>
+				<figure class="figure">
+			  <img src="img/jiso.jpg" class="figure-img img-fluid rounded-circle" alt="Testi 3">
+			</figure>
+ 		</div>
+ 		
+ 	</div>
+
+ </section>
+
+ <!--akhirtestimonal-->
+<!--Footer-->
+<div class="row footer">
+	<div class="col text-center">
+		<p>
+			2019 ALL Rights Reserved By bagasamri.
+		</p>
+		
+	</div>
+	
+</div>
+
+
+<!--akhir footer-->
+
+</div>
+   <!--Container -->
+
+
+
+   <!--akhir container-->
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
